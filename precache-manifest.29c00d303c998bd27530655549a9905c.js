956eca95f6a3f57c9643f5874543e3f4b0f08d77
stee1ix <rituraj.ranjan.42@gmail.com>
2020-12-04 00:12:17 +0530

    Updates

---
+++ precache-manifest.29c00d303c998bd27530655549a9905c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b709f084371cb20076e673b7ee14d2b5",
+    "url": "/image-recognition-webapp/index.html"
+  },
+  {
+    "revision": "18ac09343c46ef13b8d3",
+    "url": "/image-recognition-webapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6f46eb573417c975e165",
+    "url": "/image-recognition-webapp/static/css/main.0524e810.chunk.css"
+  },
+  {
+    "revision": "18ac09343c46ef13b8d3",
+    "url": "/image-recognition-webapp/static/js/2.37111a32.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/image-recognition-webapp/static/js/2.37111a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f46eb573417c975e165",
+    "url": "/image-recognition-webapp/static/js/main.829ee807.chunk.js"
+  },
+  {
+    "revision": "41994ff305d6a310e027",
+    "url": "/image-recognition-webapp/static/js/runtime-main.d2a37306.js"
+  },
+  {
+    "revision": "a3afb07ec26ca98fb42ad9726090c3eb",
+    "url": "/image-recognition-webapp/static/media/brain.a3afb07e.png"
+  }
+]);
