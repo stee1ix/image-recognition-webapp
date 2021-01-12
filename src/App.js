9b6d60b4178b617b1047d509df9449efcdfd2802
stee1ix <rituraj.ranjan.42@gmail.com>
2021-01-12 22:32:33 +0530

    deploy with link fixed

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch("https://git.heroku.com/damp-thicket-81915/imageurl", {
+    fetch("https://guarded-inlet-44047.herokuapp.com/imageurl", {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://git.heroku.com/damp-thicket-81915/image", {
+          fetch("https://guarded-inlet-44047.herokuapp.com/image", {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://git.heroku.com/damp-thicket-81915/register', {
+		fetch('https://guarded-inlet-44047.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
