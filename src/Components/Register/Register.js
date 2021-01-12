9f2b2476ba3d78a117dd81faebc118c711d601da
stee1ix <rituraj.ranjan.42@gmail.com>
2021-01-12 18:49:56 +0530

    added server link

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://git.heroku.com/damp-thicket-81915.git/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
